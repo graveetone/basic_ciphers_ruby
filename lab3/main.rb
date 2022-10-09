efb7cb40ee9ee10e680478ef07544f2ae02bcd4c
graveetone <zastavskijs@gmail.com>
2022-10-09 22:09:20 +0300

    LAB-3 added command-line args supporting

--- lab3/main.rb
+++ lab3/main.rb
@@ -1,37 +1,42 @@
+require 'optparse'
+require 'clipboard'
+
 files = %w[helpers ceasar vernam hill xor]
 
 files.each { |file| require_relative file }
 
-test_string = "Fucking encoding!"
+options = {}
+OptionParser.new do |opt|
+  opt.on('--operation OPR', String) { |o| options[:operation] = o.to_sym if o}
+  opt.on('--algorithm ALG', String) { |o| options[:algorithm] = o.to_sym if o }
+  opt.on('--phrase PHRASE', String)    { |o| options[:phrase] = o }
+  opt.on('--key KEY', String)       { |o| options[:key] = o }
+end.parse!
 
-ca = Ceasar.encode(test_string)
-puts decorated_encoded(ca)
+phrase    = options[:phrase]    || "This is test string"
+operation = options[:operation]      || :encode
+key       = options[:key]
+algorithm = options[:algorithm] || :ceasar
 
-puts
+if !key && operation == :decode
+    raise "Specify key to decode your phrase!"
+else
+    available_algorithms = {
+        ceasar: Ceasar,
+        vernam: Vernam,
+        hill:   Hill,
+        xor:    Xor
+    }
 
-dca = Ceasar.decode(ca[:result], ca[:key])
-puts decorated_decoded(dca)
+    params = [phrase]
+    params << key if key && operation != :encode
 
-va = Vernam.encode(test_string)
-puts decorated_encoded(va)
-
-puts
-
-dva = Vernam.decode(va[:result], va[:key])
-puts decorated_decoded(dva)
-
-ha = Hill.encode(test_string)
-puts decorated_encoded(ha)
-
-puts
-
-dha = Hill.decode(ha[:result], ha[:key])
-puts decorated_decoded(dha)
-
-xora = Xor.encode(test_string)
-puts decorated_encoded(xora)
-
-puts
-
-xorda = Xor.decode(xora[:result], xora[:key])
-puts decorated_decoded(xorda)
+    result = available_algorithms[algorithm].send(operation, *params)
+    if operation == :encode
+        puts decorated_encoded(result)
+        Clipboard.copy(result[:key])
+        puts "Key was copied to clipboard!".blue    
+    else
+        puts decorated_decoded(result)    
+    end
+end
